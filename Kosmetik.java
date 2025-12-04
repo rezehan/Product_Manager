9932592cc0f23162c5f57fd29023e700b2799b95
Raihan Abdillah <adraihan94@gmail.com>
2025-12-04 21:33:07 +0800

    add kosmetik class then make an object on the main file

--- Kosmetik.java
+++ Kosmetik.java
@@ -1,2 +1,9 @@
-public class Kosmetik {
+public class Kosmetik extends Product {
+    public String shade;
+
+    public void infoKosmetik() {
+        System.out.println("Ini adalah produk kosmetik " 
+        + getName() + " dengan jenis shade " 
+        + shade);
+    }
 }
--- Main.java
+++ Main.java
@@ -3,26 +3,25 @@ public class Main {
         System.out.println("======================= objek 1 ======================");
 
         // objek blush on
-        Skincare blushOn = new Skincare();
-        blushOn.jenisKulit = "kering";
-        blushOn.waktuPemakaian = "malam";
-        blushOn.kapasitas = 30;
-        blushOn.setName("mosturizer");
-        blushOn.setPrice(36000);
-        blushOn.infoSkincare();
-        blushOn.merawatKulit();
+        Skincare toner = new Skincare();
+        toner.jenisKulit = "kering";
+        toner.waktuPemakaian = "malam";
+        toner.kapasitas = 30;
+        toner.setName("mosturizer");
+        toner.setPrice(36000);
+        toner.infoSkincare();
+        toner.merawatKulit();
         
         System.out.println("======================= objek 2 ======================");
 
         // objek lipGlazed
-        Skincare lipGlazed = new Skincare();
-        lipGlazed.jenisKulit = "lembab";
-        lipGlazed.waktuPemakaian = "pagi dan malam";
-        lipGlazed.kapasitas = 20;
-        lipGlazed.merawatKulit();
+        Skincare mosturizer = new Skincare();
+        mosturizer.jenisKulit = "lembab";
+        mosturizer.waktuPemakaian = "pagi dan malam";
+        mosturizer.kapasitas = 20;
+        mosturizer.merawatKulit();
 
         System.out.println("======================= objek 3 ======================");
-
 
         // objek cusion
         Wardah cusion = new Wardah();
@@ -30,5 +29,11 @@ public class Main {
         cusion.jenisKulit = "semua jenis kulit";
         cusion.setName("cusion wardah");
         cusion.infoKategori();
+
+        System.out.println("======================= objek 4 ======================");
+        Kosmetik lipGlazed = new Kosmetik();
+        lipGlazed.shade = "APAAN NIH WKWK";
+        lipGlazed.infoKosmetik();
+
     }
 }
